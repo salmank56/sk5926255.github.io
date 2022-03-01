d7d234dff18f1be1b5b5d9747114ab2a2f2cedb5
salman4954 <sk5926255@gmail.com>
2022-03-01 15:16:53 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector('.toggle');
+const links = document.querySelector('.links');
+// Home arrow variable
+const homeArrow = document.querySelector('.home-arrow');
+
+menu.addEventListener('click', ()=>{
+    links.classList.toggle("show-links")
+})
+
+// console.log(menu)
+
+homeArrow.addEventListener('click', function(){
+    
+})
+
